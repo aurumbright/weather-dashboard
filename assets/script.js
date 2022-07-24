3319d31ddc98dc953bedb02374de9a09b99bc0a4
aurumbright <aurum.elean@gmail.com>
2022-07-23 18:38:05 -0700

    added js for sidebar list

--- assets/script.js
+++ assets/script.js
@@ -14,14 +14,28 @@ dateDisplayEl.text(currentDay);
 // inside search function: let city = document.getElementById("city").value;
 // want to make sure that city can be both the id from the search and also later, the value from a new id
 
+/* 
+Step one: Collect search query from user and request data from API: 
+- Weather icon (cloudy etc)
+- temperature
+- Wind
+- Humidity
+- UV Index
+
+Other important data: Today's date, city name in H2
+Currently holding today's date in a <span> but that might need to be done in js
+
+All of these things in a card/hero in the top row of the right side column
+*/
+
 let formSubmitHandler = function (event) {
     event.preventDefault();
     let cityName = cityInput.value.trim()
     if (cityName) {
         getCurrentWeather(cityName);
-        todayWeatherContainerEl.textContent = '';
+        // todayWeatherContainerEl.textContent = '';
         getForecast(cityName);
-        forecastContainerEl.textContent = '';
+        // forecastContainerEl.textContent = '';
     } else {
         alert('Please enter a city name');
     }
@@ -48,7 +62,7 @@ let getCurrentWeather = function (city) {
 
 }
 
-let getForecast = function(city) {
+let getForecast = function (city) {
     let queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=5" + "&appid=" + APIKey;
 
     fetch(queryURL)
@@ -112,27 +126,7 @@ var displayRepos = function (repos, searchTerm) {
 */
 
 
-
-
-
-
-
 searchFormEl.addEventListener('submit', formSubmitHandler);
-
-/* 
-Step one: Collect search query from user and request data from API: 
-- Weather icon (cloudy etc)
-- temperature
-- Wind
-- Humidity
-- UV Index
-
-Other important data: Today's date, city name in H2
-Currently holding today's date in a <span> but that might need to be done in js
-
-All of these things in a card/hero in the top row of the right side column
-*/
-
 
 
 /*
@@ -145,6 +139,9 @@ Step two: Get next five days' forecast for bottom row of right side column
 - UV Index
 */
 
+
+
+
 /*
 Step three: Store previous searches in list below search form in left-hand column
 */
@@ -152,5 +149,67 @@ Step three: Store previous searches in list below search form in left-hand colum
 /*
 Step four: Reconstitute previous searches in right-hand column 
     when previous search buttons/list items are clicked
+  */
 
-*/
+// Adds previous searches to sidebar list
+let searchList = document.querySelector("#searches");
+let cities = [];
+
+function listCities() {
+
+    searchList.innerHTML = "";
+
+    for (var i = 0; i < cities.length; i++) {
+        let prevCity = cities[i];
+
+
+        // This will need to be fixed: I don't want to just make them links, I want them to run their searches
+        let a = document.createElement('a');
+        a.href = "";
+
+
+        let list = document.createElement("li");
+        list.textContent = prevCity;
+        list.setAttribute("data-index", i);
+
+        a.appendChild(list)
+        searchList.appendChild(a);
+    }
+}
+
+function init() {
+    let storedCities = JSON.parse(localStorage.getItem("cities"));
+
+    if (storedCities !== null) {
+        cities = storedCities;
+    }
+    listCities();
+}
+
+function storeCities() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+searchFormEl.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let cityText = cityInput.value.trim();
+    if (cityText === "") {
+        return;
+    }
+    cities.push(cityText);
+    cityInput.value = '';
+    storeCities();
+    listCities();
+});
+
+searchList.addEventListener("click", function (event) {
+    let element = event.target;
+    if (element.matches("button") === true) {
+        let index = element.parentElement.getAttribute("data-index");
+        cities.splice(index, 1);
+        storeCities();
+        listCities();
+    }
+})
+
+init();
