ff830cfcb1c2d4f4f6091a0f9b623c23ff422cda
aurumbright <aurum.elean@gmail.com>
2022-07-23 18:44:34 -0700

    updated js and fixed typos

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+const APIKey = "23d272a09ce577ff8776ec8c3049893b";
 const searchFormEl = document.querySelector("#user-form");
 
 let cityInput = document.getElementById("city");
@@ -85,45 +86,6 @@ let getForecast = function (city) {
 let displayWeather = function () {
 
 }
-
-/* 
-
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
-    return;
-  }
-
-  repoSearchTerm.textContent = searchTerm;
-
-  for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-    var repoEl = document.createElement('a');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-    repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
-};
-*/
 
 
 searchFormEl.addEventListener('submit', formSubmitHandler);
